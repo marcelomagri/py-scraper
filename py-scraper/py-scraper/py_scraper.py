37942d3eeb57ad12e619a5257d078d2e4d2af3e2
Marcelo N. Magri <marcelo_magri@hotmail.com>
2019-05-20 15:31:55 -0300

    gravação no banco de dados dos itens coletados

--- py-scraper/py-scraper/py_scraper.py
+++ py-scraper/py-scraper/py_scraper.py
@@ -2,6 +2,8 @@ import schedule
 import time
 from bs4 import BeautifulSoup
 import requests
+import sqlite3
+import datetime
 
 # variáveis globais
 items_to_scrape = 120
@@ -13,37 +15,58 @@ googleplay_produtos = []
 def msg():
     print("Oi")
 
+def openConn():
+    conn = sqlite3.connect('data/dados.db')
+    cursor = conn.cursor()
+
+    return conn, cursor
+
+def closeConn(conn):
+
+    conn.close()
+
 def scrape():
     print("scraping...")
 
-    url = "https://play.google.com/store/apps/category/GAME_WORD/collection/topselling_free?start=51&num=50&gl=%s"
-    origem = requests.get(url % "br").text
-    html = BeautifulSoup(origem, 'html.parser')
-    produtos = html.find_all('div', {'class': ['card-content']})
+    db, cursor = openConn()
 
-    for produto in produtos:
-        print(str(produto.attrs['data-docid']))
-        titulo_produto = produto.find('a', {'class': ['title']}).text
-        classificacao = str.replace(str.split(titulo_produto)[0], '.', '')
-        detalhes_produto = [str(produto.attrs['data-docid']), classificacao, 'br']
-        googleplay_produtos.append(detalhes_produto)
+    for indice in indices:
+        url = "https://play.google.com/store/apps/category/GAME_WORD/collection/topselling_free?start=%s&num=%s&gl=%s"
+        origem = requests.get(url % (str(indice),items_to_scrape,"br")).text
+        html = BeautifulSoup(origem, 'html.parser')
+        produtos = html.find_all('div', {'class': ['card-content']})
 
-    time.sleep(15)
+        for current, produto in enumerate(produtos):
+            atual, descricao = str(current), str(produto.attrs['data-docid'])
+            print('%s: %s', (atual, descricao))
+            titulo_produto = produto.find('a', {'class': ['title']}).text
+            classificacao = str.replace(str.split(titulo_produto)[0], '.', '')
+            detalhes_produto = [str(produto.attrs['data-docid']), classificacao, 'br']
+            googleplay_produtos.append(detalhes_produto)
+            cursor.execute("INSERT INTO RAW_DATA (PACKAGE, DESCRIPTION, POSITION, ORIGIN, DATAHORA) VALUES ('%s', '%s', %s, 'br', '%s')" % (descricao, titulo_produto[titulo_produto.find('.  ') + 3:999], classificacao, str(datetime.datetime.utcnow())))
+            db.commit()
 
-    origem = requests.get(url % "us").text
-    html = BeautifulSoup(origem, 'html.parser')
-    produtos = html.find_all('div', {'class': ['card-content']})
+        time.sleep(15)
 
-    for produto in produtos:
-        print(str(produto.attrs['data-docid']))
-        titulo_produto = produto.find('a', {'class': ['title']}).text
-        classificacao = str.replace(str.split(titulo_produto)[0], '.', '')
-        detalhes_produto = [str(produto.attrs['data-docid']), classificacao, 'us']
-        googleplay_produtos.append(detalhes_produto)
+        origem = requests.get(url % (str(indice),items_to_scrape,"us")).text
+        html = BeautifulSoup(origem, 'html.parser')
+        produtos = html.find_all('div', {'class': ['card-content']})
 
-    time.sleep(15)
+        for current, produto in enumerate(produtos):
+            atual, descricao = str(current), str(produto.attrs['data-docid'])
+            print('%s: %s', (atual, descricao))
+            titulo_produto = produto.find('a', {'class': ['title']}).text
+            classificacao = str.replace(str.split(titulo_produto)[0], '.', '')
+            detalhes_produto = [str(produto.attrs['data-docid']), classificacao, 'us']
+            googleplay_produtos.append(detalhes_produto)
+            cursor.execute("INSERT INTO RAW_DATA (PACKAGE, DESCRIPTION, POSITION, ORIGIN, DATAHORA) VALUES ('%s', '%s', %s, 'us', '%s')" % (descricao, titulo_produto[titulo_produto.find('.  ') + 3:999], classificacao, str(datetime.datetime.utcnow())))
+            db.commit()
 
-schedule.every(5).seconds.do(msg)
+        time.sleep(15)
+
+    closeConn(db)
+
+schedule.every(5).minutes.do(msg)
 
 while True:
     # schedule.run_pending()
