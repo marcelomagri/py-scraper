a6b76e3287cce492c2b97c0d69d51dba25d3863b
Marcelo N. Magri <marcelo_magri@hotmail.com>
2019-05-21 16:01:26 -0300

    otimizações na captura dos apps via post inclusão e teste de loja PT

--- py-scraper/py-scraper/py_scraper.py
+++ py-scraper/py-scraper/py_scraper.py
@@ -6,10 +6,11 @@ import sqlite3
 import datetime
 
 # variáveis globais
-items_to_scrape = 120
+items_to_scrape = 100
 intervalo = 30
-indices = range(0, items_to_scrape * 3 + 1, items_to_scrape)
-paises = ['br', 'us']
+start_item = 0
+indices = range(start_item, start_item + (items_to_scrape * 4), items_to_scrape)
+paises = ['pt'] #'br', 'us'
 googleplay_produtos = []
 
 def msg():
@@ -25,6 +26,9 @@ def closeConn(conn):
 
     conn.close()
 
+def repeat_to_length(string_to_expand, length):
+   return (string_to_expand * ((length/len(string_to_expand))+1))[:length]
+
 def scrape():
     print("scraping...")
 
@@ -32,22 +36,34 @@ def scrape():
 
     for country_code in paises:
         for indice in indices:
-            url = "https://play.google.com/store/apps/category/GAME_WORD/collection/topselling_free?start=%s&num=%s&gl=%s"
-            origem = requests.get(url % (str(indice),items_to_scrape,country_code)).text
+            #url = "https://play.google.com/store/apps/category/GAME_WORD/collection/topselling_free?start=%s&num=%s&gl=%s"
+            url = "https://play.google.com/store/apps/category/GAME_WORD/collection/topselling_free"
+            print("Capturando %s" % url)
+            #origem_raw = requests.get(url % (str(indice),items_to_scrape,country_code), headers = {'User-agent': 'appai bot 0.1'})
+            origem_raw = requests.post(url, {"start":str(indice), "num":str(items_to_scrape), "xhr":"1", "numChildren":"0", "gl": country_code})
+
+            if origem_raw.status_code != 200:
+                print("Erro: %s" % str(origem_raw.status_code))
+            origem = origem_raw.text
             html = BeautifulSoup(origem, 'html.parser')
             produtos = html.find_all('div', {'class': ['card-content']})
 
             for current, produto in enumerate(produtos):
                 atual, descricao = str(current), str(produto.attrs['data-docid'])
-                print('%s: %s', (atual, descricao))
-                titulo_produto = produto.find('a', {'class': ['title']}).text
+                print(str(current + indice))
+                # print('%s: %s', (atual, descricao))
+                titulo_produto = str.replace(produto.find('a', {'class': ['title']}).text, '\'', '\'\'')
                 classificacao = str.replace(str.split(titulo_produto)[0], '.', '')
                 detalhes_produto = [str(produto.attrs['data-docid']), classificacao, 'br']
                 googleplay_produtos.append(detalhes_produto)
-                cursor.execute("INSERT INTO RAW_DATA (PACKAGE, DESCRIPTION, POSITION, ORIGIN, DATAHORA) VALUES ('%s', '%s', %s, 'br', '%s')" % (descricao, titulo_produto[titulo_produto.find('.  ') + 3:999], classificacao, str(datetime.datetime.utcnow())))
+                cursor.execute("INSERT INTO RAW_DATA (PACKAGE, DESCRIPTION, POSITION, ORIGIN, DATAHORA) VALUES ('%s', '%s', %s, '%s', '%s')" % (descricao, titulo_produto[titulo_produto.find('.  ') + 3:999], classificacao, country_code, str(datetime.datetime.utcnow())))
                 db.commit()
 
-            time.sleep(15)
+            for ix in range(180, 0, -1):
+                print("Esperando por %i segundos antes de efetuar o novo request \r" % ix, end="")
+                time.sleep(1)
+
+            print("")
 
             #origem = requests.get(url % (str(indice),items_to_scrape,"us")).text
             #html = BeautifulSoup(origem, 'html.parser')
@@ -67,9 +83,13 @@ def scrape():
 
     closeConn(db)
 
-schedule.every(5).minutes.do(msg)
+# schedule.every(5).minutes.do(msg)
 
 while True:
     # schedule.run_pending()
     scrape()
-    time.sleep(1)
+    #qtd = 120
+    #valores = range(0, 360, 120)
+    #for val in valores:
+    #    print(val)
+    time.sleep(60)
